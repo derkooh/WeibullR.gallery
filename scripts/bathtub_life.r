820ff2466ea1b39d11b86e267efb61eb38f1d504
Todd Takala <foo@forloop.run>
2020-11-10 08:03:59 -0700

    updated 994 wheels

--- scripts/bathtub_life.r
+++ scripts/bathtub_life.r
@@ -1,7 +1,7 @@
 require(WeibullR)
-agc<-read.csv("https://raw.githubusercontent.com/openrelia/WeibullR.gallery/master/data/acid_gas_compressor.csv", header=T)$agc
-dafit<-wblr(agc, label="acid gas compressor")
+agc<-read.csv("994-wheels.csv", header=T)$wheels
+dafit<-wblr(agc, label="994-wheels")
 dafit<-wblr.fit(dafit,col="red")
 plot(dafit, main="Bathtub Life Data") 
-abline(v=107, col="orange", lty=5, lwd=2)
-abline(v=1750, col="orange", lty=5, lwd=2) 
+abline(v=3000, col="orange", lty=5, lwd=2)
+abline(v=8061, col="orange", lty=5, lwd=2) 
